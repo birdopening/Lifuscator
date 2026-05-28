40dd74b17e2d280897d1e55a7f03d1dec96ecb7b
bird <264618980+birdopening@users.noreply.github.com>
2026-05-28 20:10:48 +0200

    Added ASM dependencies

--- lifuscator-core/build.gradle.kts
+++ lifuscator-core/build.gradle.kts
@@ -13,6 +13,13 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:6.0.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // Asm
+    implementation("org.ow2.asm:asm:9.10.1")
+    implementation("org.ow2.asm:asm-commons:9.10.1")
+    implementation("org.ow2.asm:asm-util:9.10.1")
+    implementation("org.ow2.asm:asm-tree:9.10.1")
+    implementation("org.ow2.asm:asm-analysis:9.10.1")
 }
 
 tasks.test {
