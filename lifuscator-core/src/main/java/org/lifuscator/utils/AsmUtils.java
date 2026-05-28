a3f2602b7635ec7d1364e60d52427e1c1bee3bb0
bird <264618980+birdopening@users.noreply.github.com>
2026-05-28 21:12:54 +0200

    Create AsmUtils.java

---
+++ lifuscator-core/src/main/java/org/lifuscator/utils/AsmUtils.java
@@ -0,0 +1,33 @@
+package org.lifuscator.utils;
+
+import lombok.experimental.UtilityClass;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
+
+@UtilityClass
+public class AsmUtils {
+
+    public String findUnusedMethodName(ClassNode clazz, String desiredName, String desc) {
+        if (!doesThisMethodExist(clazz, desiredName, desc)) {
+            return desiredName;
+        }
+
+        String name;
+        int i = 0;
+
+        do {
+            name = desiredName + i++;
+        } while (doesThisMethodExist(clazz, name, desc));
+
+        return name;
+    }
+
+    public boolean doesThisMethodExist(ClassNode clazz, String name, String desc) {
+        for (MethodNode method : clazz.methods) {
+            if (method.name.equals(name) && method.desc.equals(desc)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
