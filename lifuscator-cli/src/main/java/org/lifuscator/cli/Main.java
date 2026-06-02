e8929203aa85f7b917a365bbac4cd95ea206278c
Lars <264618980+birdopening@users.noreply.github.com>
2026-06-02 20:51:21 +0200

    logback cfg

--- lifuscator-cli/src/main/java/org/lifuscator/cli/Main.java
+++ lifuscator-cli/src/main/java/org/lifuscator/cli/Main.java
@@ -4,7 +4,16 @@ import org.lifuscator.core.context.Context;
 
 public class Main {
 
+    public static final String ASCII_ART = """
+             __    __  ____ __ __  __    ___  ___  ______   ___   ____\s
+             ||    || ||    || || (( \\  //   // \\\\ | || |  // \\\\  || \\\\
+             ||    || ||==  || ||  \\\\  ((    ||=||   ||   ((   )) ||_//
+             ||__| || ||    \\\\_// \\_))  \\\\__ || ||   ||    \\\\_//  || \\\\
+                                                                       \
+            """;
+
     public static void main(String[] args) {
+        System.out.println(ASCII_ART);
         Context context = new Context(args[0], args[1]);
         context.run();
     }
